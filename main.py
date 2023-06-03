d80230def51ff69072eeec90f56048bd1ec3e4e6
Matheus Scomparin <matheusadamo@unifei.edu.br>
2023-06-03 17:45:16 -0300

    MVP

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello word")
+import requests
+from bs4 import BeautifulSoup
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def GetDolar():
+  url = ['https://dolarhoje.com/', 'https://www.google.com/search?q=dolar+hoje&oq=dolar&aqs=chrome.0.35i39i650j69i59j0i67i433i650l2j0i67i650j0i67i433i650j0i67i131i433i650j69i60.681j0j4&sourceid=chrome&ie=UTF-8']
+  headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+  atributos = [{'id':'nacional'}, {'class':'DFlfde SwHCTb'}]
+  returns = ['value', 'data-value']
+  i = 0
+
+
+  page = requests.get(url[i], headers=headers)
+  print(page.content)
+
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+ 
+  dollar = soup.find_all('input', attrs=atributos[i])[0]
+
+  return {'dollar': dollar[returns[i]]}
+
